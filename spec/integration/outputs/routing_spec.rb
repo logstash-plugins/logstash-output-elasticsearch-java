8805ee9de594cd54e38791d5294e403daf047add
Andrew Cholakian <andrew@andrewvc.com>
2015-09-21 10:48:48 -0500

    Add network_host to specs

--- spec/integration/outputs/routing_spec.rb
+++ spec/integration/outputs/routing_spec.rb
@@ -51,8 +51,8 @@ describe "(transport protocol) index events with fieldref in routing value", :in
           protocol => "transport"
           index => "#{index}"
           flush_size => #{flush_size}
-          routing => "%{message}",
-          network_host => #{get_local_host}
+          routing => "%{message}"
+          network_host => "#{get_local_host}"
         }
       }
       CONFIG
